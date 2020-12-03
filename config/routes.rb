5aa8b8d4a61dca33adb1cda4c2c3e22aec9a34a0
mizuguchitomoko <shuikouzhizi7@gmail.com>
2020-12-03 12:17:20 +0000

    blogコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/new'
+  get 'blogs/edit'
+  devise_for :directors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
