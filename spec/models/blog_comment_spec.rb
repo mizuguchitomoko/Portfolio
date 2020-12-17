6299106225c83e08441db88476a815a912275f3f
mizuguchitomoko <shuikouzhizi7@gmail.com>
2020-12-17 08:19:37 +0000

    favicon

--- spec/models/blog_comment_spec.rb
+++ spec/models/blog_comment_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 RSpec.describe BlogComment, type: :model do
 
   it "名前、コメントがある場合、有効である" do
-    blog_comment = BlogComment.new(
-    comment_name: "taro",
-    blog_comment: "Hello,world"
+    blog = Blog.new
+    blog_comment = blog.blog_comments.new(
+    comment_name: "アリス",
+    blog_comment: "Hello,world",
     )
     expect(blog_comment).to be_valid
   end
 
   it "名前がない場合、無効である" do
-    blog_comment = BlogComment.new(
+    blog = Blog.new
+    blog_comment = blog.blog_comments.new(
     comment_name: nil,
     blog_comment: "Hello,world"
     )
@@ -19,8 +21,9 @@ RSpec.describe BlogComment, type: :model do
   end
 
   it "コメントがない場合、無効である" do
-    blog_comment = BlogComment.new(
-    comment_name: "taro",
+    blog = Blog.new
+    blog_comment = blog.blog_comments.new(
+    comment_name: "アリス",
     blog_comment: nil
     )
     expect(blog_comment.valid?).to eq false;
