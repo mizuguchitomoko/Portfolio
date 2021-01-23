d3da3a3ebcb101ded339a478edf36748bdc6a8f8
mizuguchitomoko <shuikouzhizi7@gmail.com>
2021-01-23 06:09:10 +0000

    refactoring

--- app/controllers/directors/contacts_controller.rb
+++ app/controllers/directors/contacts_controller.rb
@@ -2,7 +2,7 @@ class Directors::ContactsController < ApplicationController
 before_action :authenticate_director!
   def index
     @contacts = Contact.page(params[:page]).per(6)
-    @notification_count = check_notifications
+    @notification_count = Notification.checked_count
   end
 
   def show
@@ -26,15 +26,6 @@ before_action :authenticate_director!
   end
 
   private
-  def check_notifications
-    #未確認の通知(checked:falseの通知)を示すunchecked_notificationsメソッド
-    unchecked_notifications = Notification.where(checked:false)
-    #indexを開いたらchecked:falseが全てchecked:trueになる
-    unchecked_notifications.each do |un|
-      un.update!(checked: true)
-    end
-    return unchecked_notifications.size
-  end
   def contact_params
     params.require(:contact).permit(:status)
   end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,16 @@ class Notification < ApplicationRecord
   default_scope->{order(created_at: :desc)}
   #optional: trueはでnilを許可
   belongs_to :contact, optional: true
+
+  #クラスメソッド
+  def self.confirmed
+    unchecked_notifications = where(checked: false)
+    unchecked_notifications.each do |un|
+      un.update!(checked: true)
+    end
+    unchecked_notifications
+  end
+  def self.checked_count
+    confirmed.count
+  end
 end
